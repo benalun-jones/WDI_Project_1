b6d3404d2b1d80a4a84bc0f5584bf102bbcbc75d
benalun-jones <benalunjones@gmail.com>
2015-10-12 14:34:43 +0100

    creatingArrayOfSounds

--- LetsPlay/play.js
+++ LetsPlay/play.js
@@ -1,11 +1,65 @@
 
 window.onload = function() {
 
-  document.getElementById("play").addEventListener("click", check);
- 
-function check(){
-  console.log ("working")
+// document.getElementById("play").addEventListener("click", check);
 
+// function check(){
+//   console.log ("working")
+//   }
+
+// function play(){
+//   console.log ("music")
+//   var audio = document.getElementById("audio");
+//     audio.play();
+// }
+
+document.getElementById("play").addEventListener("click", playMusic);
+
+
+audio.clip = number [18];
+audio.Play();
+
+
+var  audioClips = [
+  "Ace_Hood_Flow.m4a", 
+  "Buzzin.m4a", 
+  "Candy_Shop.m4a", 
+
+
+var number = [zero, one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen, fourteen]
+
+
+
+var songSelecet: AudioClip[
+  track ('Ace_Hood_Flow', true, '../sounds/Ace_Hood_Flow.m4a')
+
+
+];
+
+<audio id="sound" src="../sounds/Ace_Hood_Flow.m4a">
+<audio id="sound" src="../sounds/Buzzin.m4a">
+<audio id="sound" src="../sounds/Candy_Shop.m4a">
+<audio id="sound" src="../sounds/Harder_Than_You_Think.m4a">
+<audio id="sound" src="../sounds/Hey_Porsche.m4a">
+<audio id="sound" src="../sounds/High_Hopes.m4a">
+<audio id="sound" src="../sounds/Ho_Hey.m4a">
+<audio id="sound" src="../sounds/Jubel.mp3">
+<audio id="sound" src="../sounds/Let_Her_Go.m4a">
+<audio id="sound" src="../sounds/Mess_Is_Mine.m4a">
+<audio id="sound" src="../sounds/On_Top.m4a">
+<audio id="sound" src="../sounds/Pompeii.m4a">
+<audio id="sound" src="../sounds/See_You_Again.m4a">
+<audio id="sound" src="../sounds/The_Nights.m4a">
+<audio id="sound" src="../sounds/The_Other_Side.m4a">
+<audio id="sound" src="../sounds/Velvet_Elvis.m4a">
+<audio id="sound" src="../sounds/Wake_Me_Up.m4a">
+<audio id="sound" src="../sounds/Window_Shopper.m4a">
+
+
+function playMusic(){
+  var audioarray = document.getElementsByTagName("audio");
+  var songSelect = Random.Range(0,audioArray.length)
+  songSelect.play();
 }
 
 
@@ -17,9 +71,7 @@ function check(){
 
 
 
-
-
-
+// <input type="img" value="play"  onclick="play()">
 
 
 
