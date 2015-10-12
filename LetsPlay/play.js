2685fae3d51aab3a808607ca019a9e4bb2a1db5c
benalun-jones <benalunjones@gmail.com>
2015-10-12 11:15:56 +0100

    testedClickListener

---
+++ LetsPlay/play.js
@@ -0,0 +1,27 @@
+
+window.onload = function() {
+
+  document.getElementById("play").addEventListener("click", check);
+ 
+function check(){
+  console.log ("working")
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
