12f73e470cd2e742f4b313319b08fa6808fcdc39
benalun-jones <benalunjones@gmail.com>
2015-10-14 10:43:26 +0100

    added_ReadMe

--- LetsPlay/play.js
+++ LetsPlay/play.js
@@ -4,19 +4,20 @@ document.getElementById("play").addEventListener("click", playMusic);
 
 var songSelect;
 var whichPlayer = 1; // true is player 1, false is player 2
+// create 2 variables player1Score and player2score, and increment the number of points 
 var player1Score = 0;
 var player2Score = 0;
 
   function buttonCallback(event) {
     var index = Array.prototype.slice.call(buttons).indexOf(event.target);
-
+//
     soundManager.stop(songSelect.filename);
     if(songSelect.answers[index].isCorrect) {
       alert("Correct!");
       if(whichPlayer == true){
         player1Score++
         var playerOne = document.getElementById('Player1').innerText = player1Score;
-
+        //create a function that, when an answer is clicked, will alert whether you have clicked the correct answer
       } else {
         player2Score++
         document.getElementById('Player2').innerText = player2Score ;
@@ -25,18 +26,17 @@ var player2Score = 0;
       alert("You know NOTHING!");
     }
     CheckForWinner()
+    //this function changes which players turn it is next
     console.log("before", whichPlayer);
     whichPlayer = whichPlayer == 1 ? 2 : 1;
     console.log("after", whichPlayer);
+    //and alerts the user this information
     alert("Player " + whichPlayer + " to Play next!")
   }
 
-// in the dom, add some information about which player is meant to play
-// create 2 variables player1Score and player2score, and increment the number of points 
-// for each time they answer correctly
 
 var buttons = document.getElementsByTagName("button");
-
+//add an event listener to each button option that will link with the funcitons below
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", buttonCallback);
   //function where, if neither player has reached 4, switch player 
@@ -47,6 +47,8 @@ for (var i = 0; i < buttons.length; i++) {
 var buttons = document.getElementsByTagName("button");
 var songs = window.songs;
 
+//a function that refers to the songs.js file which stores attributes of each song
+//these include the filename, url and possible answers
 function playMusic(){
   var songIndex = Math.floor(Math.random()*songs.length);
   songSelect = window.songs[Math.floor(Math.random()*songs.length)];
@@ -54,36 +56,34 @@ function playMusic(){
     id: songSelect.filename,
     url: songSelect.url
   });
-
+//a function that will play the song on the click of a button an the play icon
   sound.play();
 
   for (var i = 0; i < buttons.length; i++) {
 
     buttons[i].innerHTML=songSelect.answers[i].answer;
     
-    //function where, if neither player has reached 4, switch player 
-    //and repeat the entire process (do i need to create a function of the whole code, and get that to run again?)
+    
+    
   }
 }
 
+//a function that links the players score log with the scoreboard on the page
 function updateScoreBoard(){
   document.getElementById('Player1').innerText = Player1Score;
   document.getElementById('Player2').innerText = Player2Score;
   }
   
-
+//function to check for winner if either player has reached 4
+//an alert that will inform them if the function terms are met
 function CheckForWinner(){
     if(player1Score === 4 || player2Score === 4){
       alert("You Win!")
       location.reload();
     }
   }
-} // window onload 
+} 
 
-// function updateScoreBoard(){
-//   document.getElementById('humanScore').innerText = userScore;
-//   document.getElementById('computerScore').innerText = botScore;
-// }
 
 
 
@@ -104,12 +104,7 @@ function CheckForWinner(){
 
 // document.getElementsByTagName("button").addEventListener("click", stopMusic);
 
-  // function stopMusic(){
-  //   console.log('check')
-  //   soundManager.stop(songSelect.filename);
-  //   buttons.index()
-  //   // console.log(songSelect.answers);
-  // }
+
 
 // var q1 = Object.create(PossibleAnswers)
 // q1.answers = ['Ace Hood Flow (Skepta)', 'That’s not me (Skepta)', 'Hate Me Now (Nas)']
--- README.md
+++ README.md
@@ -1,8 +1,39 @@
 # WDI_Project_1: Beat The Intro
 
+# This Project was done in week 2/3 of the Web Development Immersive course that commenced on 28th September 2015.
+
+#It was achieved in 3 days, and presented to the group on completion
+
 # Project Idea: To create a game that tests the players music knowledge.  Players take it in turns to guess the song that is playing, recieving a point for every correct answer they give.
 
 #The Code:
 
-Basic site with three webpages (homepage, instructions page, and the Play Page)
+Basic site with three webpages (homepage, instructions page, and the Play Page)- each with separate html and CSS files
 
+-A basic function that is executed on the click of the play icon button.  
+-This function then runs the others in turn including:
+-One to call a random song from a library of sounds in a separate folder
+-One to play this song
+-A for loop to enable myself to add an event listener on each one.  
+-Alerts notify the player whether he/she is right or worng
+-function to check for a winner
+-and notify a player when he/she has won the game
+-a function that implements this on a scoreboard
+
+
+<!-- name of the projects and all sub-modules and libraries (sometimes they are named different and very confusing to new users)
+descriptions of all the project, and all sub-modules and libraries
+5-line code snippet on how its used (if it's a library)
+copyright and licensing information (or "Read LICENSE")
+instruction to grab the documentation
+instructions to install, configure, and to run the programs
+instruction to grab the latest code and detailed instructions to build it (or quick overview and "Read INSTALL")
+list of authors or "Read AUTHORS"
+instructions to submit bugs, feature requests, submit patches, join mailing list, get announcements, or join the user or dev community in other forms
+other contact info (email address, website, company name, address, etc)
+a brief history if it's a replacement or a fork of something else
+legal notices (crypto stuff)
+
+
+
+ -->
